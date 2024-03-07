7059cfc6396e828eda23348ff6c20b524c2e0c0b
emilianogastaldo <emiliano0303@gmail.com>
2024-03-07 17:02:22 +0100

    added main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,23 @@
 
 @section('main')
 
-<div>
-    Ciao dsd
-</div>
+<main>
+    <section class="jumbotron">
+    </section>
+    <section class="wrapper flex flex-column position-relative">
+        <div class="row">
+            @foreach ($comics=config('comics') as $comic)                
+            <div class="card">
+                <div class="card-img">
+                    <img src="{{asset($comic['thumb'])}}" alt="{{$comic['series']}}">
+                </div>
+                <div class="card-text">{{$comic['series']}}</div>
+            </div>
+            @endforeach
+        </div>
+        <a href="" class="btn btn-more">LOAD MORE</a>
+        <div class="btn btn-label"> CURRENT SERIES</div>
+    </section>
+</main>
     
 @endsection
