72027d34188a152e26b01edcd38422496e8219cf
emilianogastaldo <emiliano0303@gmail.com>
2024-03-07 16:13:56 +0100

    fixed header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,17 +1,27 @@
-
-<header>
-    <div class="wrapper flex">
-        <figure>
-            <a href="#"><img src="{{asset('images/dc-logo.png')}}" alt="DC Logo"></a>
-        </figure>
-        <nav>
-            <ul class="flex">
-                @foreach ( $links = config('header_links') as $link )
-                <li>
-                    <a href="{{$link['url']}}">{{$link['text']}}</a>
-                </li>                    
-                @endforeach
-            </ul>
-        </nav>
-    </div>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>    
+    <header>
+        <div class="wrapper flex">
+            <figure>
+                <a href="#"><img src="{{asset('images/dc-logo.png')}}" alt="DC Logo"></a>
+            </figure>
+            <nav>
+                <ul class="flex">
+                    @foreach ( $links = config('header_links') as $link )
+                    <li>
+                        <a href="{{$link['url']}}">{{$link['text']}}</a>
+                    </li>                    
+                    @endforeach
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
