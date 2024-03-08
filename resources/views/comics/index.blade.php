74097e463aa849372815172a0465ee0adf1da5ae
emilianogastaldo <emiliano0303@gmail.com>
2024-03-08 16:24:42 +0100

    added links to imgs

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,12 +9,14 @@
     </section>
     <section class="wrapper flex flex-column position-relative">
         <div class="row">
-            @foreach ($comics=config('comics') as $comic)                
+            @foreach ($comics=config('comics') as $index => $comic)                
             <div class="card">
-                <div class="card-img">
+                <a href="{{route('comics', $index)}}">
+                    <div class="card-img">
                     <img src="{{asset($comic['thumb'])}}" alt="{{$comic['series']}}">
-                </div>
-                <div class="card-text">{{$comic['series']}}</div>
+                    </div>
+                    <div class="card-text">{{$comic['series']}}</div>
+                </a>
             </div>
             @endforeach
         </div>
