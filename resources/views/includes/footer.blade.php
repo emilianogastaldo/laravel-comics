96ef0546cfd56be991485e50249db3bc72fb7422
emilianogastaldo <emiliano0303@gmail.com>
2024-03-08 10:24:46 +0100

    fixed markup

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,46 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <footer>
-        <section id="footer-top" class="wrapper">
+<footer>
+    <section id="footer-top" class="wrapper">
             <div class="row">
-                @foreach ($menus=config('footer_menu') as $menu)                    
-                <div class="col">
-                    <h4>{{ $menu['title'] }}</h4>
-                    <ul>
-                        @foreach ($menu['links'] as $link)                            
-                        <li>
-                            <a href="{{$link['href']}}">{{ $link['text'] }}</a>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endforeach
+            @foreach ($menus=config('footer_menu') as $menu)                    
+            <div class="col">
+                <h4>{{ $menu['title'] }}</h4>
+                <ul>
+                    @foreach ($menu['links'] as $link)                            
+                    <li>
+                        <a href="{{$link['href']}}">{{ $link['text'] }}</a>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
-        </section>
-        <section id="footer-bottom">
-            <nav class="wrapper flex">
-                <div id="footer-bottom-nav-left">
-                    <a href="#" class="btn btn-signup">SIGN-UP NOW!</a>
-                </div>
-                <div id="footer-bottom-nav-right">
-                    <ul>
-                        <li><a href="#" class="follow-btn">FOLLOW US</a></li>
-                        @foreach ($icons=config('footer_icons') as $icon)
-                        <li>
-                            <a href="{{$icon['href']}}"><img src="{{asset('images/' . $icon['img'])}}" alt="{{$icon['name']}}"></a>
-                        </li>                            
-                        @endforeach
-                    </ul>
-                </div>
-            </nav>
-        </section>
-    </footer>
-</body>
-</html>
+            @endforeach
+        </div>
+    </section>
+    <section id="footer-bottom">
+        <nav class="wrapper flex">
+            <div id="footer-bottom-nav-left">
+                <a href="#" class="btn btn-signup">SIGN-UP NOW!</a>
+            </div>
+            <div id="footer-bottom-nav-right">
+                <ul>
+                    <li><a href="#" class="follow-btn">FOLLOW US</a></li>
+                    @foreach ($icons=config('footer_icons') as $icon)
+                    <li>
+                        <a href="{{$icon['href']}}"><img src="{{asset('images/' . $icon['img'])}}" alt="{{$icon['name']}}"></a>
+                    </li>                            
+                    @endforeach
+                </ul>
+            </div>
+        </nav>
+    </section>
+</footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous'/>
-</head>
-<body>    
-    <header>
-        <div class="wrapper flex">
-            <figure>
-                <a href="#"><img src="{{asset('images/dc-logo.png')}}" alt="DC Logo"></a>
-            </figure>
-            <nav>
-                <ul class="flex">
-                    @foreach ( $links = config('header_links') as $link )
-                    <li>
-                        <a href="{{route($link['url'])}}" class="{{Route::is($link['url'])? 'active' : ''}}">{{$link['text']}}</a>
-                    </li>                    
-                    @endforeach
-                </ul>
-            </nav>
-            <form>
-                <input type="text" placeholder="Search">
-                <button><i class="fa-solid fa-magnifying-glass"></i></button>
-            </form>
-        </div>
-    </header>
-</body>
-</html>
+<header>
+    <div class="wrapper flex">
+        <figure>
+            <a href="#"><img src="{{asset('images/dc-logo.png')}}" alt="DC Logo"></a>
+        </figure>
+        <nav>
+            <ul class="flex">
+                @foreach ( $links = config('header_links') as $link )
+                <li>
+                    <a href="{{route($link['url'])}}" class="{{Route::is($link['url'])? 'active' : ''}}">{{$link['text']}}</a>
+                </li>                    
+                @endforeach
+            </ul>
+        </nav>
+        <form>
+            <input type="text" placeholder="Search">
+            <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+    </div>
+</header>
